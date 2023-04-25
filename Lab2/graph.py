347e1cf4dc44f36eea749f6b9a40785f05964010
kamal0620040 <neupane0620040@outlook.com>
2023-04-25 20:32:09 +0545

    Added Lab 2

--- Lab2/graph.py
+++ Lab2/graph.py
@@ -1,39 +1,56 @@
 import random
 import copy
-from time import time
+import time
 from matplotlib import pyplot as plt
 from sorting import MergeSort, InsertionSort
  
-data_setsize = [50, 100, 500, 1000, 2500, 5000, 7500, 10000]
+dataset_size = [1000, 2500, 5000, 7500, 10000]
 dataset = []
  
-def generate_dataset():
-    for i in data_setsize:
-        temp = []
-        for _ in range(0, i):
-            temp.append(random.randint(0, i))
-        dataset.append(temp)
+for size in dataset_size:
+    # random.sample(list, no_of_element) => it will return list that contain random 
+    # number of element from the given list
+    dataset.append(random.sample(range(1, size*10), size))
+
  
-generate_dataset()
- 
-insertion_sort_times = [0] * len(data_setsize)
-merge_sort_times = [0] * len(data_setsize)
+insertion_sort_times = [0] * len(dataset_size)
+merge_sort_times = [0] * len(dataset_size)
  
 def calculate_time():
-    for i in range(0,len(data_setsize)):
+    for i in range(0,len(dataset_size)):
+        
         # for insertion sort
         temp1 = copy.copy(dataset[i]) # creating deep copy
-        start_time = time()
+        
+        # sorting the already sorted list - best case
+        # temp1 = sorted(temp1)
+        # start_time = time.time_ns()
+        # InsertionSort(temp1)
+        # end_time = time.time_ns()
+
+        # sorting the reversly sorted list - Worst case
+        temp1 = sorted(temp1, reverse=True)
+        start_time = time.time_ns()
         InsertionSort(temp1)
-        end_time = time()
-        # print(end_time-start_time)
+        end_time = time.time_ns()
+
         insertion_sort_times[i] = end_time - start_time
  
         # for merge sort
         temp2 = copy.copy(dataset[i])
-        start_time = time()
-        MergeSort(temp2,0,data_setsize[i]-1)
-        end_time = time()
+        
+        # sorting the already sorted list - best case
+        # temp2 = sorted(temp2)
+        # start_time = time.time_ns()
+        # MergeSort(temp2,0,dataset_size[i]-1)
+        # end_time = time.time_ns()
+
+        # sorting the reversly sorted list - worst case
+        temp2 = sorted(temp2, reverse=True)
+        start_time = time.time_ns()
+        MergeSort(temp2,0,dataset_size[i]-1)
+        end_time = time.time_ns()
+        
         # print(end_time-start_time)
         merge_sort_times[i] = end_time - start_time
  
@@ -42,6 +59,10 @@ calculate_time()
 print(insertion_sort_times)
 print(merge_sort_times)
  
-plt.plot(data_setsize,insertion_sort_times,color="blue")
-plt.plot(data_setsize,merge_sort_times,color="red")
+plt.plot(dataset_size,insertion_sort_times,color="blue",label="Insertion Sort")
+plt.plot(dataset_size,merge_sort_times,color="red", label="Merge Sort")
+plt.xlabel('Amount of data')
+plt.ylabel('Time taken (ns)')
+plt.title('Search algorithm performance')
+plt.legend()
 plt.show()
--- Lab2/test_sorting.py
+++ Lab2/test_sorting.py
@@ -3,16 +3,30 @@ from sorting import MergeSort, InsertionSort
 
 class SearchTestCase(unittest.TestCase):
     def test_insertion_sort(self):
-        input = [5,4,3,2,1]
-        output = [1,2,3,4,5]
-        InsertionSort(input)
-        self.assertListEqual(input, output)
+        # when the input is reverse sorted
+        input1 = [5,4,3,2,1]
+        output1 = [1,2,3,4,5]
+        InsertionSort(input1)
+        self.assertListEqual(input1, output1)
+        
+        # when the input is already sorted
+        input2 = [1,2,3,4,5]
+        output2 = [1,2,3,4,5]
+        InsertionSort(input2)
+        self.assertListEqual(input2, output2)
 
     def test_merge_sort(self):
-        input = [5,4,3,2,1]
-        output = [1,2,3,4,5]
-        MergeSort(input,0,len(input)-1)
-        self.assertListEqual(input, output)
+        # when the input is reverse sorted
+        input1 = [5,4,3,2,1]
+        output1 = [1,2,3,4,5]
+        MergeSort(input1,0,len(input1)-1)
+        self.assertListEqual(input1, output1)
+
+        # when the input is already sorted
+        input2 = [1,2,3,4,5]
+        output2 = [1,2,3,4,5]
+        MergeSort(input2, 0, len(input2) - 1)
+        self.assertListEqual(input2, output2)
 
 if __name__ == "__main__":
     unittest.main()
